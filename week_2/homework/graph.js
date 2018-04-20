3edd6d18281018b4cb33f05e8eed958f277b128b
joosthintzen40 <36224997+joosthintzen40@users.noreply.github.com>
2018-04-20 18:39:27 +0200

    final commit week 2

--- week_2/homework/graph.js
+++ week_2/homework/graph.js
@@ -1,16 +1,22 @@
 /*
 * Joost Hintzen
 * 10434143
-*
+* dataprocessing week 2
 */
-function reqListener() {
 
+// function for making the graph
+function load() {
+
+  // gets the data for data.text
   let dataBilt = this.responseText;
-  console.log(dataBilt);
+  // splits the data into arrays at whitespaces
   let splitDatas = dataBilt.split('\n');
-  //console.log(splitDatas);
+
+
   var temps = [];
   var dates = [];
+
+  // splits all arrays into 2 arrays of temps and dates
   for (var i = 0; i < splitDatas.length; i++) {
     let data = splitDatas[i].split(',');
     var temp = Number(data[1]);
@@ -19,8 +25,6 @@ function reqListener() {
     temps.push(temp);
     var datum = date.getTime();
     dates.push(datum);
-
-
   }
 
 
@@ -47,92 +51,97 @@ function reqListener() {
       }
   }
 
+  // makes 2d canvas in html file
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
-  var xBeginning = 50;
+
+  // bounds x and y coordinates of the graph
+  var xBeginning = 100;
   var xEnd = 1250;
-  var yBeginning = 950;
+  var yBeginning = 900;
   var yEnd = 50;
 
-  // x and y axis
+  // x and y axis drawn
   ctx.beginPath();
   ctx.moveTo(xBeginning, yEnd);
   ctx.lineTo(xBeginning, yBeginning);
   ctx.lineTo(xEnd, yBeginning);
-  ctx.stroke();
 
-  var distance = 0;
+  // vars for drawing x and y-axis ticks and labels
   var text = -100;
   var padding = 10;
-  var step = 100;
+  var step = (1150/12);
 
-  // y-axis ticks and labels
-
+  // y-axis ticks and labels drawn
   for (var y = yBeginning; y >= yEnd; y -= step) {
+    // labels
+    ctx.font = "20px arial";
     ctx.textAlign= "end";
     ctx.fillText(text.toString(), (xBeginning - (2*padding)), y);
-
-
-    ctx.beginPath();
-    ctx.moveTo(xBeginning, y);
-    ctx.lineTo((xBeginning - padding), y);
-    ctx.stroke();
-
     text += 50;
 
+    // ticks
+    ctx.moveTo(xBeginning, y);
+    ctx.lineTo((xBeginning - padding), y);
   }
 
+  // vars text for x-axis label
   var months = ["april", "mei", "juni", "juli", "augustus", "september",
   "oktober", "november", "december", "januari", "februari", "maart", "april"];
   var i = 0;
 
-  // x -axis and labels
-  ctx.beginPath();
+  // x -axis and labels drawn
   for (var x = xBeginning; x <= xEnd; x += step) {
-    ctx.beginPath();
-    ctx.moveTo(x, yBeginning);
-    ctx.lineTo(x, (yBeginning + padding));
-    ctx.stroke();
+    // labels
+    ctx.textAlign = "center";
     ctx.fillText(months[i], x, (yBeginning + (2*padding)));
     i++;
+
+    // ticks
+    ctx.moveTo(x, yBeginning);
+    ctx.lineTo(x, (yBeginning + padding));
   }
 
+  // vars to transform data into coordinates
   var confy = [];
   var confx = [];
-  var datamili = [];
-  var datasY = createTransform([-100, 350], [950, 50]);
-  // datamili = dates.getTime();
-  // console.log(datamili);
-  // var firstDay = dates[0].getTime();
-  // var lastDay = dates[splitDatas.length - 1].getTime();
-  var datasX = createTransform([dates[0], dates[splitDatas.length - 1]], [50, 1250]);
+  var datasY = createTransform([-100, 350], [yBeginning, yEnd]);
+  var datasX = createTransform([dates[0], dates[splitDatas.length - 1]], [xBeginning, xEnd]);
 
-
-
+  // tranforming data into coordinates
   for (var y = 0; y < splitDatas.length; y++) {
     confy[y] = datasY(temps[y]);
     confx[y] = datasX(dates[y]);
-
   }
-  console.log(confy);
-  console.log(confx);
+  ctx.stroke();
 
+  // color graph line
   ctx.beginPath();
-  ctx.moveTo(50, confx[0]);
+  ctx.strokeStyle = "red";
+  ctx.moveTo(xBeginning, confy[0]);
 
-  for (var i = 0; i < splitDatas.length; i++) {
-
+  // drawing line graph
+  for (var i = 1; i < splitDatas.length; i++) {
     ctx.lineTo(confx[i], confy[i]);
   }
   ctx.stroke();
 
-  // ctx.beginPath();
-  // ctx.moveTo(50, confDatasA);
-  // ctx.lineTo(100, confDatasB);
-  // ctx.stroke();
+  // drawing y-axis title
+  ctx.save();
+  ctx.translate(xBeginning, yEnd);
+  ctx.rotate((Math.PI / 180) * -90);
+  ctx.fillText("Temperature (0.1 degrees Celsius)", -300, -60);
+  ctx.restore();
+
+  // drawing x-axis title
+  ctx.fillText("Months in 2017", 650, 965);
 }
 
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://raw.githubusercontent.com/joosthintzen40/dataprocessing1/master/week_2/homework/data.txt");
-oReq.send();
+// make XMLHttpRequest
+var textLoader = new XMLHttpRequest();
+// when graph.js is loaded
+textLoader.addEventListener("load", load);
+// get the data.txt out of GitHub
+textLoader.open("GET", "https://raw.githubusercontent.com/joosthintzen40/dataprocessing1/master/week_2/homework/data.txt");
+// and send it to html
+textLoader.send();
