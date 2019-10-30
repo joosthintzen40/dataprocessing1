cedc28068a53b983662888d31622ea47c03d6e41
joosthintzen40 <36224997+joosthintzen40@users.noreply.github.com>
2019-10-30 18:25:57 +0100

    push today

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,13 +1,13 @@
 import csv
 import json
 
-csvfile = open('honeyproduction.csv', 'r')
+csvfile = open('homework\week_3\honeyproduction.csv', 'r')
 jsonfile = open('honeyproduction.json', 'w')
 
-reader = csv.DictReader(csvfile, delimiter=";")
+reader = csv.DictReader(csvfile, delimiter=",")
 data = []
 
 for row in reader:
     data.append(dict(row))
 dicto = {"data": data}
-json.dump(dicto, jsonfile);
+json.dump(dicto, jsonfile)
